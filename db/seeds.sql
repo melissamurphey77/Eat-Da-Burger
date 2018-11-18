a3229b285014374ca6c144287943177da48aa500
Melissa Murphey <melissamurphey77@gmail.com>
2018-11-17 18:17:29 -0600

    adding db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', "false");
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sriracha Burger', "false");
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', "false");
