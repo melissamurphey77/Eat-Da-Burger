01f4e434bb5aa8144323025f9e6f3ced55355c90
Melissa Murphey <melissamurphey77@gmail.com>
2018-11-19 23:22:20 -0600

    updating files

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,8 @@
 var connection =require('./connection.js');
 
 var orm = {
+    
+    //GETTING BURGER DATA//
     all: function (tableInput , cb) {
         connection.query('SELECT * FROM '+tableInput+';', function(err, result) {
             if(err) throw err;
@@ -8,14 +10,24 @@ var orm = {
         })
     },
 
+    //UPDATING STATUS NOT DEVOURED/DEVOURED //
     update: function (tableInput, condition, cb) {
         connection.query('UPDATE '+tableInput+' SET devoured=true WHERE id='+condition+';',
         function(err, result) {
             if(err) throw err;
             cb(result);
         })
+    },
 
+    //INSERTING NEW BURGER INTO TABLE//
+    create: function(tableInput, val, cb) {
+        connection.query('INSERT INTO '+tableInput+' (burger.name) VALUES("+val+");',
+        function(err, result){
+            if(err)throw err;
+            cb(result);
+        })
     }
+
 }
 
 module.exports=orm;
